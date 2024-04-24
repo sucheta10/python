c08b0e6d63ac1bcb5c524802c6f9ec472cb65f89
SUCHETA MANDAL <94100111+sucheta10@users.noreply.github.com>
2024-04-24 20:43:17 +0530

    Add files via upload

---
+++ transpose.py
@@ -0,0 +1,10 @@
+mat = [[1,2],
+       [3,4],
+       [5,6]]
+r = [[0,0,0],
+     [0,0,0]]
+for i in range(len(mat)):
+   for j in range(len(mat[0])):
+       r[j][i] = mat[i][j]
+for k in r:
+   print(k)
